5bea7579dff9405be62c10b1d8a777313985f06c
leoreinmann <leo@reinmann.me>
2023-12-20 08:17:55 -0800

    Modified index.ts to use new routing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import roleRoutes from './routes/RoleRoutes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Node + TypeScript Server');
-});
+app.use(express.json())
 
+app.use(roleRoutes);
 
 app.listen(port, () => {
   console.log(`[server] app listening at http://localhost:${port}`);
