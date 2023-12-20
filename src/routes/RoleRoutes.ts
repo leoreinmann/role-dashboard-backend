6093c66503b81dd7adf2cd2ae6a26b2c659317bd
leoreinmann <leo@reinmann.me>
2023-12-20 08:17:21 -0800

    Added GET endpoint for permissions

--- src/routes/RoleRoutes.ts
+++ src/routes/RoleRoutes.ts
@@ -5,22 +5,33 @@ const router = Router();
 
 const roleService = new RoleServiceImpl();
 
+router.get('/permissions', async (req: Request, res: Response) => {
+    try {
+        const permissions = await roleService.getPermissions();
+        res.json(permissions);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
 router.get('/roles/permissions', async (req: Request, res: Response) => {
     try {
         const roles = await roleService.getRoles();
         const rolesWithPermissions = roles.map(role => {
-            const permissions = role.permissions.map(p => ({ id: p.id, name: p.name }));
+            const permissions = role.permissions ? role.permissions.map(p => ({ id: p.id, name: p.name })) : [];
             return { id: role.id, name: role.name, permissions };
         });
         res.json(rolesWithPermissions);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
 
 router.post('/roles/:roleId/permissions', async (req: Request, res: Response) => {
     const { roleId } = req.params;
-    const permissions = req.body; // Ensure request body structure is validated
+    const permissions = req.body; 
     const updatedRole = await roleService.setPermissionsForRole(roleId, permissions);
     res.json(updatedRole);
 });
